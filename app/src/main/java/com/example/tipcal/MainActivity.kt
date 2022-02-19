ae47e9ea2f703b96fe5feba80576e7f5df9cb071
Cris Castro <crcastro@wccnet.edu>
2022-02-18 21:19:58 -0500

    Project 2

---
+++ app/src/main/java/com/example/tipcal/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tipcal
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun convertCurrency(view: android.view.View) {}
+}
