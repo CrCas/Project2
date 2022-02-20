6275e0b59e83ba6a96b64a2b9b30ed8ae32ae9ff
Cris Castro <crcastro@wccnet.edu>
2022-02-20 18:48:51 -0500

    Project 2

--- app/src/main/java/com/example/tipcal/MainActivity.kt
+++ app/src/main/java/com/example/tipcal/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.tipcal
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
 import android.os.Bundle
+import android.view.View
+
+import com.example.tipcal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    fun convertCurrency(view: android.view.View) {}
+        binding.convertCurrency.setOnClickListener {
+            binding.tipView.text = "Tip Total: "
+        }
+
+        binding.convertCurrency.setOnLongClickListener {
+            binding.tipView.text = "Please enter total."
+            false
+        }
+    }
 }
